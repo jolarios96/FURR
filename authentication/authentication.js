b26f899d23f308a23db184e29becb96b1cfb8c50
jason villaluz <jasonvillaluz@jasons-MacBook-Pro.local>
2018-11-29 10:37:15 -0800

    getpet.html first round 11/29

---
+++ authentication/authentication.js
@@ -0,0 +1,16 @@
+// firebase initialization
+var config = {
+    apiKey: "AIzaSyDzixpXHU87uZ_gbAfy6YP3btwUVek9rWw",
+    authDomain: "project-furr.firebaseapp.com",
+    databaseURL: "https://project-furr.firebaseio.com",
+    projectId: "project-furr",
+    storageBucket: "project-furr.appspot.com",
+    messagingSenderId: "654872510123"
+};
+firebase.initializeApp(config);
+// --------------------------------------------------------------
+
+// database references
+var database = firebase.database();
+var user = firebase.auth().currentUser;
+
