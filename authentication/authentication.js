143c703a5ba2c3e365155079984399727672240f
Jordan Larios <jolarios96@gmail.com>
2018-11-28 16:19:17 -0800

    a little code progress on authentication.js

--- authentication/authentication.js
+++ authentication/authentication.js
@@ -1,16 +1,30 @@
-// firebase initialization
-var config = {
-    apiKey: "AIzaSyDzixpXHU87uZ_gbAfy6YP3btwUVek9rWw",
-    authDomain: "project-furr.firebaseapp.com",
-    databaseURL: "https://project-furr.firebaseio.com",
-    projectId: "project-furr",
-    storageBucket: "project-furr.appspot.com",
-    messagingSenderId: "654872510123"
-};
-firebase.initializeApp(config);
-// --------------------------------------------------------------
-
 // database references
 var database = firebase.database();
 var user = firebase.auth().currentUser;
 
+if (user) {
+    //user is signed in
+    console.log('signed in')
+}
+
+else {
+    // FirebaseUI config.
+    var uiConfig = {
+        signInSuccessUrl: 'persistence_check.html',
+        signInOptions: [
+            //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            //   firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+            firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+        ],
+    };
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
+
+}
