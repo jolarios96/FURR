4ed823c347c3d1b1bea20e0fa8590d20328b3ad9
Jordan Larios <jolarios96@gmail.com>
2018-11-30 13:41:05 -0800

    fixed update-session-data syntax

--- assets/javascript/firebase/update-session-data.js
+++ assets/javascript/firebase/update-session-data.js
@@ -5,22 +5,21 @@
 var database = firebase.database();
 
 database.ref('users/' + user.uid).on('child_added', function (snapshot) {
-    // store the data @ firebase
+    // store the data from firebase
     var name = snapshot.val().name;
     var favorites = snapshot.val().favorites;
     var offset = snapshot.val().snapshot;
     var userID = user.uid;
 
-    console.log('name: ' + name);
-    console.log('favorites: ' + favorites);
-    console.log('offset: ' + offset);
-    console.log('userID: ' + userID);
-
+    // console.log('name: ' + name);
+    // console.log('favorites: ' + favorites);
+    // console.log('offset: ' + offset);
+    // console.log('userID: ' + userID);
 
     // update session storage
-    sessionStorage.getItem('name', name);
-    sessionStorage.getItem('favorites', JSON.stringify(favorites));
-    sessionStorage.getItem('offset', offset);
-    sessionStorage.getItem('userID', userID);
+    sessionStorage.setItem('name', name);
+    sessionStorage.setItem('favorites', JSON.stringify(favorites));
+    sessionStorage.setItem('offset', offset);
+    sessionStorage.setItem('userID', userID);
 
 });
