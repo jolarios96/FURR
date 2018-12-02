cff4c452b0a446d452aecd4223c5b2236eb5020b
Jordan Larios <jolarios96@gmail.com>
2018-12-02 02:37:19 -0800

    (login.html) after user logs in, refreshes page, triggering data download from database

--- assets/javascript/download-user-data.js
+++ assets/javascript/download-user-data.js
@@ -1,6 +1,7 @@
 var database = firebase.database();
 
 firebase.auth().onAuthStateChanged(function (user) {
+
     if (user) {
         var query = firebase.database().ref("users").orderByKey();
         query.once("value")
@@ -11,23 +12,20 @@ firebase.auth().onAuthStateChanged(function (user) {
                     // childData will be the actual contents of the child
                     var childData = childSnapshot.val();
                     // if current user's id matches the snapshot's data key
+
                     if (user.uid === key) {
                         // store server data in localStorage
+                        // NOTE: array data from server is already stringified
                         localStorage.setItem('name', childData.name);
-                        localStorage.setItem('favArray', JSON.stringify(childData.favorites));
-                        localStorage.setItem('offset',  childData.offset);
+                        localStorage.setItem('favArray', childData.favorites);
+                        localStorage.setItem('offset', childData.offset);
                         localStorage.setItem('userID', user.uid);
 
-                        // sessionStorage.setItem('name', childData.name);
-                        // sessionStorage.setItem('favArray', JSON.stringify(childData.favorites));
-                        // sessionStorage.setItem('offset', childData.offset);
-                        // sessionStorage.setItem('userID', user.uid);
-
                         // console.logs
-                        // console.log('key: ' + key);
-                        // console.log('childData: ' + childData.name);
-                        // console.log('childData: ' + childData.favorites);
-                        // console.log('childData: ' + childData.offset);
+                        console.log('key: ' + key);
+                        console.log('name: ' + childData.name);
+                        console.log('favorites: ' + childData.favorites);
+                        console.log('offset: ' + childData.offset);
 
                     }
                 });
